3b69df064637d72a1abf1cd8ebe2261e559f4756
Julio Wong <julioo.wong@gmail.com>
2021-04-02 08:32:40 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
 import React from 'react'
-import Button from './components/Button'
 import MathOperations from './components/MathOperation'
 import Result from './components/Result'
 import Functions from './components/Functions'
+import Numbers from './components/Numbers'
 import './App.css';
 
 const App = () => {
-    const clickHandler = text => alert(text)
-
     return (
         <main className="react-calculator">
             <Result value={'10'}/>
-            <div className="numbers">
-                <Button text="1" clickHandler={ clickHandler }/>
-                <button>2</button>
-                <button>3</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>0</button>
-            </div>
+            <Numbers onClickNumber={ number => console.log(number) } />
             <Functions 
                 onContentClear={() => console.log("Conten Clear")}
                 onDelete={() => console.log("Conten Clear")}/>
